4a74dd4255a0cd28a226ec382ce10abb68b7a0f4
grove86ajinkya <ajinkya@jbktechnologies.com>
2021-11-02 18:09:51 +0530

    change in db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,13 @@ const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
 //creating connection to databse
-const conn = mysql.createConnection({
+// const conn = mysql.createConnection({
+//     host: dbConfig.HOST,
+//     user: dbConfig.USER,
+//     password: dbConfig.PASSWORD,
+//     database: dbConfig.DB
+// });
+var conn = mysql.createPool({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
@@ -11,9 +17,9 @@ const conn = mysql.createConnection({
 
 //opening connection
 
-conn.connect(err => {
-    if(err) throw err;
-    console.log("Connected to database "+ dbConfig.DB);
-});
+// conn.connect(err => {
+//     if(err) throw err;
+//     console.log("Connected to database "+ dbConfig.DB);
+// });
 
 module.exports = conn;
